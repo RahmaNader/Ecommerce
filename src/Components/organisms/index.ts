2244ac2b068f4b3a9b54001cab4bf3bc83d985ee
Hesham Seleem <hishamsayed444@gmail.com>
2024-10-24 03:25:42 +0300

    pulling dev

--- src/Components/organisms/index.ts
+++ src/Components/organisms/index.ts
@@ -1,2 +1,3 @@
-export { default as Navbar} from './Navbar/Navbar'
-export { default as ShopModal} from './ShopModal/ShopModal'
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as ShopModal } from "./ShopModal/ShopModal";
+export { default as Cart } from "./Cart/Cart";
