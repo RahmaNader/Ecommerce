6900eb907dbe89d9cd2f6d077b0edc5cb87fe6d1
Rahma Nader <r.nader@egac.rak.ae>
2024-11-14 10:41:52 +0200

    initially finished the basics of the shop feature

--- src/pages/Screens/Shop/Shop.tsx
+++ src/pages/Screens/Shop/Shop.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useState}   from "react";
 import { useParams, Navigate } from "react-router-dom";
 import {Filter} from '@components/organisms';
 import { Breadcrumb } from "@components/molecules";
@@ -112,40 +112,53 @@ const cards = [
 ];
 
 const Shop: React.FC = () => {
-  const { category, item } = useParams<ShopParams>();
+  const { category, item } = useParams();
+  const [filteredCards, setFilteredCards] = useState(cards);
 
   if (!category) {
     return <Navigate to="/" />;
   }
 
+   // Extract the last segment of the URL path
+  const lastSegment = location.pathname.split("/").filter(Boolean).pop();
+
   return (
-    <div className="flex flex-col lg:flex-row bg-customBeige min-h-screen mb-10 items-start justify-center">
-      <div className="ml-10 mt-16">
-        <Breadcrumb />
-        <Filter/>
-      </div>
-      
-      <div className="flex flex-col md:flex-row flex-wrap gap-5 justify-items-center ">
-        <div className="w-[100%]">
-          <p className="font-playball text-[40px] text-wine">
-            Tops
-          </p>
+    <div className="bg-customBeige min-h-screen md:p-10">
+       {/* <Breadcrumb /> */}
+      <div className="flex flex-col lg:flex-row items-start ">
+        {/* Filter Section */}
+        <div className="w-full md:w-1/4 p-4 md:sticky md:top-0 flex justify-center">
+          <Filter onFilterChange={() => {
+            // Handle filter change logic here
+            const filtered = cards.filter(card => {
+              return card.rate > 3; 
+            });
+            setFilteredCards(filtered);
+          }} />
         </div>
-        {cards.map((card) => (
-          <div
-            key={card.name}
-            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex justify-center my-10"
-          >
-            <Card
-              src={card.src}
-              alt={card.alt}
-              name={card.name}
-              DisPrice={card.DisPrice}
-              NormalPrice={card.NormalPrice}
-              rate={card.rate}  
-            />
+        {/* Products Section */}
+        <div className="w-full lg:w-3/4 p-4">
+        <p className="font-playball text-[40px] text-wine text-center md:text-left">
+        {(lastSegment ?? '').charAt(0).toUpperCase() + (lastSegment ?? '').slice(1)}
+      </p>
+          <div className="flex flex-wrap gap-5 justify-center">
+            {filteredCards.map((card) => (
+              <div
+                key={card.name}
+                className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 flex justify-center my-10"
+              >
+                <Card
+                  src={card.src}
+                  alt={card.alt}
+                  name={card.name}
+                  DisPrice={card.DisPrice}
+                  NormalPrice={card.NormalPrice}
+                  rate={card.rate}
+                />
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
