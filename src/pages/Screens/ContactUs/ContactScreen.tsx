a51a18dbc2e8f2c4a5b36ff9883a25017768b7f7
Ali-khaled99 <alikhaled2226@gmail.com>
2024-11-17 14:07:01 +0200

    update contact section for pull request

--- src/pages/Screens/ContactUs/ContactScreen.tsx
+++ src/pages/Screens/ContactUs/ContactScreen.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import phoneIcon from '../../../assets/PhoneIcon.svg'
-import letterIcon from '../../../assets/LetterIcon.svg'
+import phoneIcon from "../../../assets/PhoneIcon.svg";
+import letterIcon from "../../../assets/LetterIcon.svg";
 import { Button } from "@components/atoms";
-import { useForm, Controller, SubmitHandler, FieldError  } from 'react-hook-form';
+import {
+  useForm,
+  Controller,
+  SubmitHandler,
+  FieldError,
+} from "react-hook-form";
 
 type formFields = {
   name: string;
   email: string;
   phone: number;
   message: string;
-}
+};
 
 const ContactScreen: React.FC = () => {
-
-  const { control, handleSubmit, formState: { errors } } = useForm<formFields>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<formFields>();
 
   const onSubmit: SubmitHandler<formFields> = (data) => {
     console.log(data);
-  }
+  };
 
   const getErrorMessage = (error: FieldError | undefined) => {
     return error?.message ? error.message : null;
@@ -50,7 +58,9 @@ const ContactScreen: React.FC = () => {
                 <span className="ms-4">Write To US</span>
               </div>
               <div className="mt-4 ">
-                <p>Fill out our form and we will contact you within 24 hours.</p>
+                <p>
+                  Fill out our form and we will contact you within 24 hours.
+                </p>
               </div>
               <div className="mt-4">
                 <p>Emails: customer@exclusive.com</p>
@@ -62,7 +72,6 @@ const ContactScreen: React.FC = () => {
           </div>
         </div>
         <div className="xl:w-2/3 lg:w-2/3 md:w-1/2 sm:w-full xs:w-full border border-skin bg-[#A78E7821] p-10 rounded-lg sm:px-4 xs:px-4 md:h-[550px] lg:h-[450px]">
-
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex justify-between gap-3 xs:flex-col sm:flex-col  md:flex-col lg:flex-row xl:flex-row xl:justify-between">
               <div className="lg:w-1/3">
@@ -70,10 +79,18 @@ const ContactScreen: React.FC = () => {
                   name="name"
                   control={control}
                   rules={{ required: "Name is required" }}
-                  render={({ field }) => <input {...field} id="name" placeholder="Your Name"
-                    className='bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin md:w-full xs:w-full sm:w-full focus:border-skin focus:text-skin' />}
+                  render={({ field }) => (
+                    <input
+                      {...field}
+                      id="name"
+                      placeholder="Your Name"
+                      className="bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin md:w-full xs:w-full sm:w-full focus:border-skin focus:text-skin"
+                    />
+                  )}
                 />
-                {errors.name && <span>{getErrorMessage(errors.name as FieldError)}</span>}
+                {errors.name && (
+                  <span>{getErrorMessage(errors.name as FieldError)}</span>
+                )}
               </div>
               <div className="lg:w-1/3 xs:w-full">
                 <Controller
@@ -83,22 +100,40 @@ const ContactScreen: React.FC = () => {
                     required: "Email is required",
                     pattern: {
                       value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                      message: "Invalid email address"
-                    }
+                      message: "Invalid email address",
+                    },
                   }}
-                  render={({ field }) => <input {...field} id="email" placeholder="Your Email" className='bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin md:w-full xs:w-full sm:w-full focus:border-skin focus:text-skin' />}
+                  render={({ field }) => (
+                    <input
+                      {...field}
+                      id="email"
+                      placeholder="Your Email"
+                      className="bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin md:w-full xs:w-full sm:w-full focus:border-skin focus:text-skin"
+                    />
+                  )}
                 />
-                {errors.email && <span>{getErrorMessage(errors.email as FieldError)}</span>}
+                {errors.email && (
+                  <span>{getErrorMessage(errors.email as FieldError)}</span>
+                )}
               </div>
               <div className="lg:w-1/3 xs:w-full">
                 <Controller
                   name="phone"
                   control={control}
                   rules={{ required: "Phone number is required" }}
-                  render={({ field }) => <input {...field} id="phone" placeholder="Your Phone" className='bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin xs:w-full sm:w-full  md:w-full focus:border-skin focus:text-skin' />}
+                  render={({ field }) => (
+                    <input
+                      {...field}
+                      id="phone"
+                      placeholder="Your Phone"
+                      className="bg-[#A78E7821] border border-skin px-3 py-3 rounded-md placeholder:text-skin xs:w-full sm:w-full  md:w-full focus:border-skin focus:text-skin"
+                    />
+                  )}
                 />
                 <div>
-                  {errors.phone && <span>{getErrorMessage(errors.phone as FieldError)}</span>}
+                  {errors.phone && (
+                    <span>{getErrorMessage(errors.phone as FieldError)}</span>
+                  )}
                 </div>
               </div>
             </div>
@@ -107,23 +142,29 @@ const ContactScreen: React.FC = () => {
                 name="message"
                 control={control}
                 rules={{ required: "Message is required" }}
-                render={({ field }) => <textarea {...field} id="message" placeholder="Message" className="w-full h-[200px] bg-[#A78E7821] border border-skin p-5 mt-5 placeholder:text-skin focus:text-skin focus:border-skin " />}
+                render={({ field }) => (
+                  <textarea
+                    {...field}
+                    id="message"
+                    placeholder="Message"
+                    className="w-full h-[200px] bg-[#A78E7821] border border-skin p-5 mt-5 placeholder:text-skin focus:text-skin focus:border-skin "
+                  />
+                )}
               />
-              {errors.message && <span>{getErrorMessage(errors.message as FieldError)}</span>}
+              {errors.message && (
+                <span>{getErrorMessage(errors.message as FieldError)}</span>
+              )}
             </div>
             <div className="float-right pt-5">
               <Button
                 type="primary"
                 size="medium"
                 label="Send"
-                onClick={
-                  handleSubmit(onSubmit)
-                } />
+                onClick={handleSubmit(onSubmit)}
+              />
             </div>
-
           </form>
         </div>
-
       </div>
     </div>
   );
@@ -131,45 +172,8 @@ const ContactScreen: React.FC = () => {
 
 export default ContactScreen;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <form onSubmit={handleSubmit(onSubmit)}>
+{
+  /* <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex justify-between gap-3 max-md:flex-col md:flex-col lg:flex-row xl:flex-row xl:justify-between">
               <Input {...register("name", {
                 required: true
@@ -196,7 +200,9 @@ export default ContactScreen;
                   handleSubmit(onSubmit)
                  } />
             </div>
-          </form> */}
+          </form> */
+}
 
-
-{/* <button className="float-right pt-5" type="submit">Submit</button> */ }
+{
+  /* <button className="float-right pt-5" type="submit">Submit</button> */
+}
