a83761fbcdf81965cb8c53d647565d3761fea4b2
Rahma Nader <r.nader@egac.rak.ae>
2024-11-17 14:59:44 +0200

    pushing to work from home

--- src/Components/organisms/index.ts
+++ src/Components/organisms/index.ts
@@ -4,3 +4,4 @@ export { default as Cart } from "./Cart/Cart";
 export { default as Layout } from "./Layout/Layout";
 export {default as Home} from './Home/Home'
 export {default as Filter} from './Filter/Filter'
+export {default as ProductsDisplay} from './ProductsDisplay/ProductsDisplay'
