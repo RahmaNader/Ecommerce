a3f951d6a851e7be9b7d720f61dd4d93fe53d037
Rahma Nader <r.nader@egac.rak.ae>
2024-12-12 12:06:11 +0200

    fixed filter style and functionality in mobile and bigger screens

--- src/pages/Screens/Shop/Shop.tsx
+++ src/pages/Screens/Shop/Shop.tsx
@@ -96,25 +96,19 @@ const Shop: React.FC = () => {
           <img src={FilterIcon} alt="Open Filters" />
         </button>
       </div>
-      <div className="flex flex-col  xl:flex-row xl:items-start items-center">
-        {/* Filter Sidebar for screens smaller than laptop size */}
+
+      {/* Filter Sidebar for screens smaller than laptop size */}
+      <div className="flex flex-col xl:flex-row xl:items-start items-center">
         {showSidebar && (
           <div className="fixed inset-0 z-50 flex">
             <div
               className={`transform ${
                 isFilterOpen ? "translate-x-0" : "-translate-x-full"
-              } transition-transform duration-300 ease-in-out sm:w-3/4 bg-white p-4 overflow-y-auto `}
+              } transition-transform duration-300 ease-in-out sm:w-3/4 bg-mainColor p-4 overflow-y-auto`}
             >
-              <div className="flex flex-row items-center w-[100%] h-[36px] justify-between mt-10">
-                <p className="font-playfair text-[25px] md:text-[30px] lg:text-[35px] font-bold text-wine text-left">
-                  Filters
-                </p>
-                <button onClick={handleCloseSidebar}>
-                  <img src={FilterIcon} alt="Close Filters" />
-                </button>
-              </div>
               <div className="flex w-[100%]">
-                <Filter onFilterChange={handleFilterChange} />
+                {/* onClose passed here, so cursor will be pointer */}
+                <Filter onFilterChange={handleFilterChange} onClose={handleCloseSidebar} />
               </div>
             </div>
             {/* Overlay */}
@@ -124,10 +118,13 @@ const Shop: React.FC = () => {
             ></div>
           </div>
         )}
+
         {/* Desktop Filter for screens larger than laptop size */}
         <div className="laptop:hidden w-full md:w-1/4 p-4 md:sticky md:top-0 md:h-screen md:overflow-y-auto">
+          {/* No onClose passed here, so no pointer cursor */}
           <Filter onFilterChange={handleFilterChange} />
         </div>
+
         {/* Products Section */}
         <div className="w-full lg:w-3/4 p-4">
           <p className="font-playball text-[30px] md:text-[35px]  lg:text-[40px] text-wine text-center md:text-left">
