a9e7a92962e271ef5019bdd69db55ab264495344
Rahma Nader <r.nader@egac.rak.ae>
2024-12-13 10:44:19 +0200

    created success and error alerts

---
+++ src/Components/atoms/Alerts/ErrorAlert.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Alert from "@mui/material/Alert";
+import ErrorIcon from "@mui/icons-material/Error";
+
+const ErrorAlert: React.FC<{ message: string }> = ({ message }) => {
+  return (
+    <Alert
+      severity="error"
+      icon={
+        <ErrorIcon
+          fontSize="inherit"
+          style={{
+            backgroundColor: "white",
+            borderRadius: "50%",
+            color: "#dc2626",
+            width: "48px",
+            height: "48px",
+          }}
+        />
+      }
+      style={{
+        borderRadius: "9999px",
+        backgroundColor: "#dc2626",
+        color: "white",
+        fontSize: "1.5rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "1rem 2rem",
+        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      {message}
+    </Alert>
+  );
+};
+
+export default ErrorAlert;
---
+++ src/Components/atoms/Alerts/SuccessAlert.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Alert from "@mui/material/Alert";
+import CheckIcon from "@mui/icons-material/Check";
+
+const SuccessAlert: React.FC<{ message: string }> = ({ message }) => {
+  return (
+    <Alert
+      severity="success"
+      icon={
+        <CheckIcon
+          fontSize="inherit"
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "50%",
+            padding: "8px",
+            color: "#22c55e",
+            width: "48px",
+            height: "48px",
+          }}
+        />
+      }
+      sx={{
+        borderRadius: "9999px",
+        backgroundColor: "#22c55e",
+        color: "white",
+        fontSize: "1.5rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "1rem 2rem",
+        boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      {message}
+    </Alert>
+  );
+};
+
+export default SuccessAlert;
