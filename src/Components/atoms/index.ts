50cbb49b9a4d1298931e0c25bab2209c7721f88f
Rahma Nader <r.nader@egac.rak.ae>
2024-12-13 10:51:02 +0200

    created success and error alerts

--- src/Components/atoms/index.ts
+++ src/Components/atoms/index.ts
@@ -4,5 +4,6 @@ export { default as Button } from './Button/Button';
 export { default as NavLink } from './Link/NavLink';
 export { default as CustomRating } from './CustomRating/CustomRating';
 export { default as ProductCount } from './ProductCount/ProductCount';
+export { default as ReviewCard } from './ReviewCard/ReviewCard'
 export { default as SuccessAlert } from './Alerts/SuccessAlert'
 export { default as ErrorAlert } from './Alerts/ErrorAlert'
